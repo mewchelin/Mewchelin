9b882426afe123297d6e626d1594a798e4ef97bb
ayleeee <janhyang0309@gmail.com>
2021-11-02 23:47:11 +0900

    데이터베이스에 회원 저장, 스토리지에 이미지 저장

---
+++ app/src/main/java/com/example/android/mewchelin/UserData.kt
@@ -0,0 +1,7 @@
+package com.example.android.mewchelin
+
+data class UserData(var strName :String?=null, var uid : String?=null, var userId :String?=null,
+var imageUrl :String?=null){
+
+}
+
---
+++ app/src/main/java/com/example/android/mewchelin/checkImage.kt
@@ -0,0 +1,22 @@
+package com.example.android.mewchelin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class checkImage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_image)
+
+        val button : Button = findViewById(R.id.back)
+        button.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent);
+        }
+    }
+
+
+}
